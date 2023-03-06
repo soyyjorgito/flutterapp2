5edd212105a64f250f26050a04c0d0526dd5937f
Jorge Eslava <jorge08432@hotmail.com>
2023-03-06 15:05:07 -0500

    SimpleDialog

--- lib/src/screens/my_home_page.dart
+++ lib/src/screens/my_home_page.dart
@@ -11,33 +11,44 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       key: _scaffKey,
       appBar: AppBar(
-        title: const Text('Mostrar un Snackbar'),
+        title: const Text('Mostrar un SimpleDialog'),
       ),
       body: Center(
-        child: TextButton(
-          onPressed: () {
-            final snackBar = SnackBar(
-                backgroundColor: Colors.black,
-                content: Row(
-                  children: const [
-                    Icon(Icons.thumb_up,color: Colors.white),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text('Elemento eliminado')
-                  ],
-                ),
-                action: SnackBarAction(
-                    label: "Cancelar",
-                    onPressed: () {
-                      print("Cancelado");
-                    }));
-
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          },
-          child: const Text('Mostrar SnackBar'),
-        ),
-      ),
+          child: TextButton(
+        child: const Text("Mostrar"),
+        onPressed: () {
+          _showDialog(context);
+        },
+      )),
     );
+  }
+
+  void _showDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: const Text("Seleccionar"),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+            contentPadding: const EdgeInsets.only(left:50),
+            titlePadding: const EdgeInsets.only(left:80),
+            children: [
+              ListTile(
+                title: const Text("Eliminar"),
+                leading: const Icon(Icons.delete),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Editar"),
+                leading: const Icon(Icons.edit),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              )
+            ]
+          );
+        });
   }
 }
